ff640fc593f2013b6b7e097774c40d75d410c5b2
Sam <824404725@qq.com>
2022-08-18 23:20:50 +0800

    feat:init

---
+++ packages/components/src/YwButton/Index.vue
@@ -0,0 +1,22 @@
+<script>
+import { Button } from "element-ui";
+export default {
+  name: "YwButton",
+  data() {
+    return {};
+  },
+  props: {},
+  components: {
+    ElButton: Button,
+  },
+};
+</script>
+
+<template>
+  <el-button class="YwButton">button</el-button>
+</template>
+
+<style lang="less" scoped>
+.YwButton {
+}
+</style>
